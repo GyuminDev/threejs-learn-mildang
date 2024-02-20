e61774d5cc0d80faebc78edb7fb3679110538c45
gyumin.Sim <pleasure082@gmail.com>
2024-02-20 09:46:45 +0900

    ADD prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <ul>
       <li>
-        <Link href='/lessons/first-threejs-project'>
-          lessons/first-threejs-project
+        <Link href="/lessons/first-threejs-project">
+          03. first-threejs-project
         </Link>
       </li>
     </ul>
